530cf5bfa0e7f8d0b0c83e59a78aee59733f3506
ximias <alexx4387@gmail.com>
2019-04-23 17:11:52 +0200

    Migrated to AndroidX Added periodic notifications.

---
+++ ExerciseAPIapp/app/src/main/java/com/example/exercise_api_app/Botherer.java
@@ -0,0 +1,61 @@
+package com.example.exercise_api_app;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+public class Botherer extends Worker {
+    public static final String CHANNEL_ID = "bother";
+    public int exercisesToBother = 5;
+    public Context context;
+
+    public Botherer(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.context = context;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Botherer", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Used for reminders of old exercises");
+            NotificationManager notificationManager = getSystemService(context,NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void bother(Context context){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("Remaining exercises: "+getRemainingExercises())
+                .setContentText("You still have remaining exercises.")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        // notificationId is a unique int for each notification that you must define
+        int notificationId = 42;
+        notificationManager.notify(notificationId, builder.build());
+        Toast.makeText(context, "Bother", Toast.LENGTH_LONG).show();
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        int remainingExercises = getRemainingExercises();
+        if (remainingExercises >= exercisesToBother){
+            bother(context);
+        }
+        return Result.success();
+    }
+
+    private int getRemainingExercises() {
+        return 7;
+    }
+}
--- ExerciseAPIapp/app/src/main/java/com/example/exercise_api_app/MainActivity.java
+++ ExerciseAPIapp/app/src/main/java/com/example/exercise_api_app/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.exercise_api_app;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activateBothering();
+    }
+
+    protected void activateBothering() {
+
+        PeriodicWorkRequest br = new PeriodicWorkRequest.Builder(Botherer.class, 10, TimeUnit.HOURS).build();
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork("EBother", ExistingPeriodicWorkPolicy.REPLACE ,br);
+        Toast.makeText(this, "Bother activated!", Toast.LENGTH_LONG).show();
+        WorkManager.getInstance().cancelUniqueWork("EBother");
     }
 }
